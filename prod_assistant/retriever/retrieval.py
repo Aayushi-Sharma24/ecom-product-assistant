8b881fff71855813ae62ebf81f14f7405877adcc
Aayushi-Sharma24 <as24.aayushi@gmail.com>
2025-09-17 21:38:16 +0530

    retriever changes in import

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -2,15 +2,15 @@ import os
 from langchain_astradb import AstraDBVectorStore
 from typing import List
 from langchain_core.documents import Document
-from utils.config_loader import load_config
-from utils.model_loader import ModelLoader
+from prod_assistant.utils.config_loader import load_config
+from prod_assistant.utils.model_loader import ModelLoader
 from dotenv import load_dotenv
 import sys
 from pathlib import Path
 
 from langchain.retrievers.document_compressors import LLMChainFilter
 from langchain.retrievers import ContextualCompressionRetriever
-from evaluation.ragas_eval import evaluate_context_precision, evaluate_response_relevancy
+from prod_assistant.evaluation.ragas_eval import evaluate_context_precision, evaluate_response_relevancy
 
 # Add the project root to the Python path for direct script execution
 project_root = Path(__file__).resolve().parents[2]
